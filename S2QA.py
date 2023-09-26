c03d2d78ad363fc22ad70767608ab662b162c0ee
shauryr <shauryr@gmail.com>
2023-09-26 13:16:53 -0500

    removed questions

--- S2QA.py
+++ S2QA.py
@@ -136,7 +136,8 @@ if button and research_space_query:
             )
             full_response = str(full_response) + list_titles
             documents = st.session_state["documents"]
-            questions = display_questions(generate_sample_questions(documents))
+            # questions = display_questions(generate_sample_questions(documents))
+            questions = ""
         #     ref.push(
         #     {
         #         "full_response": full_response,
@@ -188,7 +189,8 @@ if st.session_state.get("show_chat", False):
                 )
                 full_response = str(full_response) + list_titles
                 documents = st.session_state["documents"]
-                questions = display_questions(generate_sample_questions(documents))
+                # questions = display_questions(generate_sample_questions(documents))
+                questions = ""
             else:
                 questions = ""
             message_placeholder.markdown(full_response + "\n" + questions)
